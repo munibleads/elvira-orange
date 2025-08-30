65ae334063b29a02b4af49bea332adf63048b76a
Munib Leads <munibleads@gmail.com>
2025-08-30 16:57:29 +0300

    update

--- components/CardTwo.tsx
+++ components/CardTwo.tsx
@@ -4,8 +4,8 @@ export default function CardTwo() {
   return (
     <Card className="w-full h-full flex flex-col items-center justify-center text-center p-6 bg-[#0F1016] text-white">
       <CardContent className="text-center p-10">
-        <p className="text-3xl font-bold mb-4">Identified a Plastic Water Bottle ♻️</p>
-        <p className="text-xl text-gray-200">I&apos;ll place it in the recyclable bin! Thank you for saving our planet!</p>
+        <p className="text-3xl font-bold mb-4">Please place your item in the smart bin below</p>
+        <p className="text-xl text-gray-200">The smart bin will automatically sort your item into the correct compartmnt.</p>
       </CardContent>
     </Card>
   );
